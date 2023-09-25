ccadf110d1bc773f9b91046b31a9b72875dfc710
Ravy Yim <ravyyim@Ravys-MacBook-Air.local>
2023-09-25 00:54:05 -0600

    assignment

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 
 
-// Create a solution where if the value is and even number print out even
-// When creating the solution, make sure if the number is odd, have the output print odd
-//create an if-else conditional 
-
-
-
 if("I am a string") {
     console.log("true")
 } else {
@@ -45,3 +39,42 @@ if("") {
     console.log("The empty string is falsy (the only falsy string)")
 };
 
+
+let num1 = 50;
+let num2 = 51;
+let sum = num1 + num2;
+
+if (sum < -1000) {
+    console.log(sum + " is less than -1000");
+} else if (sum < 0) {
+    console.log(sum + " is a negative number");
+} else if (sum === 0) {
+    console.log(sum + " is equal to 0");
+} else if (sum > 0 && sum <= 100) {
+    console.log(sum + " is larger than 0");
+} else {
+    console.log(sum + " is greater than 100");
+}
+
+
+let num1 = 5
+let num2 = 5
+
+
+if (num1 >= 5 && num2 >=5) {
+    console.log(`true`)
+} else {
+    console.log(`false`)
+};
+
+let param1A = "cat";
+let param1B = "cat";
+
+// Define the second set of values
+let param2A = 6;
+let param2B = "6";
+
+// Check if at least one of the pairs is truthy
+if (param1A || param1B || param2A || param2B) {
+  console.log("true");
+}
