8ef0747c02637bb2d2daa7bfad3eee6370824d8a
Ravy Yim <ravyyim@Ravys-MacBook-Air.local>
2023-09-25 00:52:38 -0600

    assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+// Create a solution where if the value is and even number print out even
+// When creating the solution, make sure if the number is odd, have the output print odd
+//create an if-else conditional 
+
+
+
+if("I am a string") {
+    console.log("true")
+} else {
+    console.log("false")
+};
+
+if(false) {
+    console.log("true")
+} else {
+    console.log("The boolean value false is falsy")
+};
+
+
+if(null) {
+    console.log("true")
+} else {
+    console.log("The null value is falsy")
+};
+
+
+if(undefined) {
+    console.log("true")
+} else {
+    console.log("Undefined is falsy")
+};
+
+
+if(0) {
+    console.log("true")
+} else {
+    console.log("The number 0 is falsy (the only falsy number)")
+};
+
+if("") {
+    console.log("true")
+} else {
+    console.log("The empty string is falsy (the only falsy string)")
+};
+
